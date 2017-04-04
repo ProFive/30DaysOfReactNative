2fe081361df06307f2e4af91974851f882c5221d
nani <ngvannam5@gmail.com>
2017-04-04 23:06:53 +0700

    Firebase

--- SimpleChat/app/Root.js
+++ SimpleChat/app/Root.js
@@ -7,24 +7,18 @@
  * @Last modified time: 04-Apr-2017
  */
 
-
 import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-  TouchableOpacity,
-  ActivityIndicator
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+    TouchableOpacity,
+    ActivityIndicator
 } from 'react-native';
 
-import {
-  LoginButton,
-  AccessToken,
-  LoginManager
-} from 'react-native-fbsdk';
-
+import {LoginButton, AccessToken, LoginManager} from 'react-native-fbsdk';
 
 import firebase from 'firebase';
 const config = {
@@ -38,63 +32,117 @@ const config = {
 firebase.initializeApp(config);
 
 class Root extends Component {
-  constructor(props) {
-   super(props);
-   const shareLinkContent = {
-     contentType: 'link',
-     contentUrl: 'https://www.facebook.com/',
-   };
+    constructor(props) {
+        super(props);
+        this.state = {
+            logged: false,
+            animating: false
+        };
+    };
+    handleLogin = () => {
+        if (!this.state.logged) {
+            LoginManager.logInWithPublishPermissions(['publish_actions']).then((result) => {
+                if (result.isCancelled) {
+                    alert('Cancel login');
+                }
+                // } else if (result.declinedPermissions) {
+                //     alert('declinedPermissions');
+                // } else {
+                this.setState({logged: true});
+                AccessToken.getCurrentAccessToken().then((data) => {
+                    alert(data.accessToken.toString())
+                }).catch(error => alert(error));
+            }).catch(error => console.log(error));
+        } else {
+            this.setState({logged: false});
+            LoginManager.logOut();
+        }
+    }
 
-   this.state = {
-     shareLinkContent: shareLinkContent,
-   };
- }
+    onLogin = async() => {
+        try {
+            this.setState({animating: true});
+            const result = await LoginManager.logInWithReadPermissions(['public_profile', 'email']);
+            const tokenData = await AccessToken.getCurrentAccessToken();
+            const token = tokenData.accessToken.toString();
+            const credential = firebase.auth.FacebookAuthProvider.credential(token);
+            const user = await firebase.auth().signInWithCredential(credential);
+            firebase.database().ref(`/users/${user.uid}/profile`).set({name: user.displayName, email: user.email, avatar: user.photoURL});
+            this.setState({animating: false});
+        } catch (error) {
+            this.setState({animating: false});
+            console.log(error.message);
+            // do something here
+        }
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                </Text>
+                <ActivityIndicator animating={this.state.animating} color="#ddd" size="large"/>
+                <LoginButton publishPermissions={["publish_actions"]} onLoginFinished={(error, result) => {
+                    if (error) {
+                        alert("login has error: " + result.error);
+                    } else if (result.isCancelled) {
+                        alert("login is cancelled.");
+                    } else {
+                        AccessToken.getCurrentAccessToken().then((data) => {
+                            alert(data.accessToken.toString())
+                        })
+                    }
+                }} onLogoutFinished={() => alert("logout.")}/>
+                <TouchableOpacity onPress={this.onLogin} style={{
+                    marginTop: 10,
+                    padding: 10,
+                    backgroundColor: 'green',
+                    borderRadius: 5
+                }}>
+                    <Text style={{
+                        color: '#fff'
+                    }}>
+                        {this.state.logged
+                            ? 'Đăng xuất'
+                            : 'Đăng nhập'}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
 
- shareLinkWithShareDialog() {
-   var tmp = this;
-   ShareDialog.canShow(this.state.shareLinkContent).then(
-     function(canShow) {
-       if (canShow) {
-         return ShareDialog.show(tmp.state.shareLinkContent);
-       }
-     }
-   ).then(
-     function(result) {
-       if (result.isCancelled) {
-         alert('Share cancelled');
-       } else {
-         alert('Share success');
-       }
-     },
-     function(error) {
-       alert('Share fail with error: ' + error);
-     }
-   );
- }
-
- render() {
-   return (
-     <View style={styles.container}>
-       <LoginButton/>
-       <TouchableHighlight style={styles.share}
-         onPress={this.shareLinkWithShareDialog.bind(this)}>
-         <Text style={styles.shareText}>Share link with ShareDialog</Text>
-       </TouchableHighlight>
-     </View>
-   );
- }
+render() {
+    return (
+        <View style={styles.container}>
+            <LoginButton/>
+            <TouchableHighlight style={styles.share} onPress={this.shareLinkWithShareDialog.bind(this)}>
+                <Text style={styles.shareText}>Share link with ShareDialog</Text>
+            </TouchableHighlight>
+        </View>
+    );
+}
 }
 export default Root;
 
 const styles = StyleSheet.create({
-  container: {
+container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  shareText: {
+    backgroundColor: '#F5FCFF'
+},
+welcome: {
     fontSize: 20,
-    margin: 10,
-  },
+    textAlign: 'center',
+    margin: 10
+},
+instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+}
 });
