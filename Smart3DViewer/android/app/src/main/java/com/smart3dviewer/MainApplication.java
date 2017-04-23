4a7e7658b48b0204724d4a1c961bedf31735c90a
nani <ngvannam5@gmail.com>
2017-04-23 14:51:19 +0700

    Update

--- Smart3DViewer/android/app/src/main/java/com/smart3dviewer/MainApplication.java
+++ Smart3DViewer/android/app/src/main/java/com/smart3dviewer/MainApplication.java
@@ -3,6 +3,8 @@ package com.smart3dviewer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
+            new RNGooglePlacesPackage(),
             new RNGoogleSigninPackage()
       );
     }
--- Smart3DViewer/src/screens/maps/Maps.js
+++ Smart3DViewer/src/screens/maps/Maps.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { DrawerButton } from '../../components';
+import MapView from 'react-native-maps';
 
 class Maps extends Component {
     static navigationOptions = {
@@ -10,11 +11,26 @@ class Maps extends Component {
             left: <DrawerButton navigation={navigation} />
         }),
     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            region: {
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }
+        };
+    }
+    onRegionChange(region) {
+        this.setState({ region });
+    }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Maps11111</Text>
-            </View>
+            <MapView
+                region={this.state.region}
+                onRegionChange={this.onRegionChange}
+            />
         );
     }
 }
